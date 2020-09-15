3a23b5e976b3073c1e2db2950b867577c0688464
quinnfinite <quinn.royston@gmail.com>
2020-09-15 16:31:22 -0700

    Added react router, and created an error component, and the beginnings of a login component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import Login from './components/Login.jsx';
+import Error from './components/Error.jsx';
 
 function App () {
   return (
-    <h1>App!</h1>
+    <main>
+      <Switch>
+        <Route path='/login' component={Login}/>
+        <Route component={Error}/>
+      </Switch>
+    </main>
+
   )
 }
 
-ReactDOM.render(<App/>, document.getElementById('app'))
+ReactDOM.render(
+<BrowserRouter>
+  <App/>
+</BrowserRouter>
+, document.getElementById('app'))
--- server/index.js
+++ server/index.js
@@ -2,11 +2,11 @@ const express = require('express');
 const app = express();
 const path = require('path')
 const port = 8000;
+const staticAssets = path.join(__dirname, '../client')
+app.use(express.static(staticAssets))
 
-app.use(express.static(path.join(__dirname, '../client')))
-
-app.get('/', (req, res) => {
-  res.sendFile('index')
+app.get('*', (req, res) => {
+  res.sendFile('index.html', {root: staticAssets})
 })
 
 app.listen(port, ()=>{
