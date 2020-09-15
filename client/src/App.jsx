b6647039da87e4ee2c3b2b9460372a2041d0a7da
quinnfinite <quinn.royston@gmail.com>
2020-09-15 16:46:36 -0700

    added success component to router in App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import Login from './components/Login.jsx';
 import Signup from './components/Signup.jsx';
+import Success from './components/Success.jsx';
 import Error from './components/Error.jsx';
 
 function App () {
@@ -11,6 +12,7 @@ function App () {
       <Switch>
         <Route path='/login' component={Login}/>
         <Route path='/signup' component={Signup}/>
+        <Route path='/success' component={Success}/>
         <Route component={Error}/>
       </Switch>
     </main>
